cce8cf078877d923127fbc34de0bfa0ab6b6c22b
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2019-11-05 11:39:05 +0000

    Added jsnip

---
+++ jsnip/src/main/java/dev/marcelo/java/Caller.java
@@ -0,0 +1,30 @@
+package dev.marcelo.java;
+
+import dev.marcelo.java.pck.Callee;
+import dev.marcelo.java.pck.MEnum;
+
+public class Caller {
+  private int someInt;
+  private final MEnum e = MEnum.ENUMA;
+
+  public Caller(int someInt) {
+    this.someInt = someInt;
+  }
+
+  public int getSomeInt() {
+    return someInt;
+  }
+
+  public void setSomeInt(int someInt) {
+    this.someInt = Callee.someInt + someInt;
+  }
+
+  public void testAPI() {
+    int k = getSomeInt();
+    DummyAPI api = new DummyAPI(k);
+    RandomAPI rapi = new RandomAPI();
+    String s = rapi.toString();
+    setSomeInt(api.getNum());
+  }
+
+}
